2ab0da00884516e4ba37a7a071ea0146853e4ef8
Modurotolu Olokode <50303829+durotolu@users.noreply.github.com>
2026-03-02 13:08:59 +0100

    added products seed

--- product-service/src/index.ts
+++ product-service/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectDB } from "./config/db";
 import productRoutes from "./routes/product.routes";
+import { seedProducts } from "./seed/seed";
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use("/products", productRoutes);
 
 const start = async () => {
   await connectDB();
+  await seedProducts()
 
   app.listen(3001, () =>
     console.log("Product Service running")
