7b65e9b41740429ca8b0eccfd7c96775ebc6c659
Modurotolu Olokode <50303829+durotolu@users.noreply.github.com>
2026-03-04 15:21:25 +0100

    updated morker test set up and dockerfile

--- transaction-worker/jest.config.js
+++ transaction-worker/jest.config.js
@@ -1,11 +1,12 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   testMatch: ['**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   transform: {
-    '^.+\\.ts?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+    '^.+\\.ts?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   }
 };
